7cc7e6e4d7e5196f40e545dc1e7726e88185002a
Gurjeevan <gurjeevanmalhi@outlook.com>
2025-09-20 16:09:45 -0500

    loaded clean fx rate table

---
+++ clean_table_loads/load_fx_rates_clean.sql
@@ -0,0 +1,16 @@
+-- Creating clean exchange rate table
+create table fx_rates
+(
+    fx_date  date,
+    currency varchar(3),
+    fx_rate  decimal(5, 4)
+);
+
+-- Inserting cleaning data
+insert into fx_rates(fx_date, currency, fx_rate)
+select fx_date::date, currency, fx_rate::decimal(5, 4)
+from exchange_rates_staging;
+
+-- Dropped staging table
+drop table if exists exchange_rates_staging cascade;
+
